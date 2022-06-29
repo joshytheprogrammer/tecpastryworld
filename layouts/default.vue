f9123c77c4e0c5f61e01f89bc5fcebe37e756bf1
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-06-29 03:44:01 +0100

    created layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,45 @@
 <template>
-  
+  <div class="app">
+    <MNavigation v-if="mobile" />
+    <DNavigation v-else />
+    <Nuxt />
+  </div>
 </template>
 
 <script>
-export default {
+import DNavigation from "../components/App/Nav.vue" // Desktop Navbar
+import MNavigation from "../components/App/MobileNav.vue" // Mobile Navbar
 
+import { mapMutations, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters({
+      mobile: 'mobile/mobile',
+    }),
+  },
+  components: {
+    DNavigation,
+    MNavigation
+  },
+  mounted() {
+    this.isMobile()
+
+    // Watch for window size change
+    window.addEventListener("resize", this.isMobile);
+  },
+  unmounted() {
+    window.removeEventListener("resize", this.isMobile);
+  },
+  methods: {
+    ...mapMutations({
+      isMobile: 'mobile/checkWidth',
+    }),
+  },
+  
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
