fe73b6296abe0be757e65c76f390808eab9c1b43
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-06-29 03:58:10 +0100

    created Nav structure

---
+++ components/App/Helpers/Desktop/SearchBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bar">
+    <input type="text">
+    <button>Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- components/App/Nav.vue
+++ components/App/Nav.vue
@@ -1,15 +1,26 @@
 <template>
   <nav>
-
+    <NuxtLink to="/" class="logo" title="Go home">tec</NuxtLink>
+    <Bar />
+    <Links />
   </nav>
 </template>
 
 <script>
+import Bar from "./Helpers/Desktop/SearchBar.vue"
+import Links from "./Helpers/Desktop/NavLinks.vue"
 export default {
-
+  components: {
+    Bar,
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+nav {
 
+  .logo {
+    @include logo-styles;
+  }
+}
 </style>
