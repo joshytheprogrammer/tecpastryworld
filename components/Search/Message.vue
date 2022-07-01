0113d93f4969a9880cf9f5a1a8cc29c608939b58
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-01 11:42:51 +0100

    finsihed building first_message component

---
+++ components/Search/Message.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="message">
+    <p>Showing over <b>{{total}}</b> results for <b><span>"{{term}}"</span></b></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["total", "term"]
+}
+</script>
+
+<style lang="scss" scoped>
+.message {
+  width: 100%;
+  background: $light;
+  padding: 1rem;
+  border-radius: 8px;
+
+  p {
+    font-weight: 500;
+    font-size: 16px;
+
+    span {
+      color: $secondary;
+    }
+  }
+}
+</style>
--- pages/s/index.vue
+++ pages/s/index.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
-    {{$route.query.k}}
+  <div class="search">
+    <!-- Pass data down as props -->
+    <FirstMessage :total="search.total" :term="search.term" />
+    <!-- Search for cakes here, use the SearchCard component to render it here -->
   </div>
 </template>
 
 <script>
+import FirstMessage from "~/components/Search/Message.vue"
 export default {
-
+  components: {
+    FirstMessage
+  },
+  
+  data(){
+    return {
+      search: {
+        total: 130,
+        term: this.$route.query.k,
+        results: [],
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.search {
+  padding: 0.5rem;
+}
 </style>
