1799c82eab45620651359a0d965374dfbbc5f7bf
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-02 13:51:54 +0100

    preparing mobile search

--- store/search.js
+++ store/search.js
@@ -1,12 +1,16 @@
 export default {
   namespaced: true,
   state : () => ({
-    
+    searching: false,
   }),
   mutations: {
-    
+    handleSearchBar(state) {
+      state.searching = !state.searching
+    }
   },
   getters: {
-    
+    isSearchOpen(state) {
+      return state.searching
+    }
   }
 }
