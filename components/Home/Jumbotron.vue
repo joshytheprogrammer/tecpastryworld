8fdfeb74869d1607d5f1c3062e5046be20ec4b11
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-05 17:57:47 +0100

    trying second approach

--- components/Home/Jumbotron.vue
+++ components/Home/Jumbotron.vue
@@ -1,7 +1,5 @@
 <template>
-  <section class="jumbotron">
-
-  </section>
+  <section class="jumbotron"></section>
 </template>
 
 <script>
@@ -11,5 +9,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.jumbotron {
+  padding: 1rem;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-color: $secondary;
+  object-fit: fill;
+  height: 700px;
+  max-width: 100%;
+  color: $light;
 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  @media screen and (max-width: $medium) {
+    background-color: $primary;
+    padding-bottom: 2rem;
+    height: 600px;
+  }
+}
 </style>
