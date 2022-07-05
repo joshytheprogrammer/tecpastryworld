4861f849149e2ee8b45814a16f8ffa22ccfd7474
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-05 18:27:38 +0100

    finished jumbotron

--- components/Home/Jumbotron.vue
+++ components/Home/Jumbotron.vue
@@ -1,5 +1,9 @@
 <template>
-  <section class="jumbotron"></section>
+  <section class="jumbotron">
+    <h1>Welcome to the official TECpastryworld website</h1>
+    <p>Here you can find the best products and deals we offer.</p>
+    <NuxtLink to="/shop">Shop Now</NuxtLink>
+  </section>
 </template>
 
 <script>
@@ -13,11 +17,12 @@ export default {
   padding: 1rem;
   background-attachment: fixed;
   background-repeat: no-repeat;
-  background-color: $secondary;
+  background-color: #ff914d;
   object-fit: fill;
-  height: 700px;
+  height: 600px;
   max-width: 100%;
   color: $light;
+  user-select: none;
 
   display: flex;
   flex-direction: column;
@@ -25,10 +30,61 @@ export default {
   justify-content: center;
   text-align: center;
 
+  h1 {
+    font-weight: 600;
+    font-size: 48px;
+    padding: 1rem 0;
+  }
+
+  p { 
+    line-height: 1.235;
+    font-size: 28px;
+    padding: 1rem 0;
+  }
+
+  a {
+    display: block;
+    margin: 1rem 0;
+    width: fit-content;
+    background: $light;
+    color: $primary;
+    padding: 12px 16px;
+    text-decoration: none;
+    border-radius: 8px;
+
+    &:hover {
+      background: $primary;
+      color: $light;
+    }
+  }
+
   @media screen and (max-width: $medium) {
-    background-color: $primary;
     padding-bottom: 2rem;
-    height: 600px;
+    height: 500px;
+
+    h1 {
+      font-size: 36px;
+      font-weight: 500;
+    }
+
+    p {
+      font-size: 24px;
+      padding: 1rem 0;
+    }
+  }
+
+  @media screen and (max-width: $small) {
+    padding-bottom: 2rem;
+
+    h1 {
+      font-size: 28px;
+      font-weight: 400;
+    }
+
+    p {
+      font-size: 18px;
+      padding: 1rem 0;
+    }
   }
 }
 </style>
