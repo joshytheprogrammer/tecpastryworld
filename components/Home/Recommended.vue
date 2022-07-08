a0532890b97855bd19720f41b8403ee09fda229f
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-08 20:23:38 +0100

    created Recommended component and started fetching

---
+++ components/Home/Recommended.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="recommended">
+    <!-- <div v-for="item in recommended" :key="item.id">
+      <img :src="item.thumbnail" width="250px" height="250px" alt="">
+      <p>{{item._id}}</p>
+      <p>{{item._slug}}</p>
+      <p>{{item.name}}</p>
+      <p>{{JSON.parse(item.price).lowest + "-" + JSON.parse(item.price).highest}}</p>
+    </div> -->
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  data() {
+    return {
+      recommended: []
+    }
+  },
+  async fetch() {
+    await axios.get('http://127.0.0.1:8000/api/home/recommended').then((response) => {
+      this.recommended.push(...response.data.data)
+    })
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="home">
     <Jumbotron />
+    <Recommended />
   </div>
 </template>
 
 <script>
 import Jumbotron from "../components/Home/Jumbotron.vue"
+import Recommended from "../components/Home/Recommended.vue"
 export default {
   components: {
-    Jumbotron
+    Jumbotron,
+    Recommended
   }
 }
 </script>
