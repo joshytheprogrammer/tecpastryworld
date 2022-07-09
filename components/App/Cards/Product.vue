7914912d87ee73b009c0c59898f3eb2b0751b8a7
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-09 12:58:49 +0100

    improved recommended and product pages. created mobile form of products card not seperate file

--- components/App/Cards/Product.vue
+++ components/App/Cards/Product.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="product">
-      <img loading="lazy" :src="item.thumbnail" :alt="item.name">
-      <div class="_details">
-        <NuxtLink :to="'/shop/'+item._slug">{{item.name}}</NuxtLink>
-        <span class="prices">{{JSON.parse(item.price).lowest + "-" + JSON.parse(item.price).highest}}</span>
-      </div>
+    <img loading="lazy" :src="item.thumbnail" :alt="item.name">
+    <div class="_details">
+      <NuxtLink :to="'/shop/'+item._slug">{{item.name}}</NuxtLink>
+      <span class="prices">{{JSON.parse(item.price).lowest + "-" + JSON.parse(item.price).highest}}</span>
+    </div>
   </div>
 </template>
 
@@ -25,10 +25,10 @@ export default {
     border-radius: 10px;
     transition: 0.5s all ease-in-out;
 
-    &:hover {
-      // Make image zoom properly later
-      // transform: scale(1.5);
-    }
+    // &:hover {
+    //   // Make image zoom properly later
+    //   transform: scale(1.5);
+    // }
   }
 
   ._details {
@@ -42,7 +42,7 @@ export default {
       color: inherit;
       text-decoration: none;
       font-size: 21px;
-      font-weight: 500;
+      font-weight: 600;
 
       &:hover {
         text-decoration: underline;
@@ -53,6 +53,37 @@ export default {
     span {
       font-size: 18px;
       font-weight: 400;
+    }
+  }
+
+  @media screen and (max-width: $s-medium) {
+    display: inline-flex;
+    border: 1px solid $primary;
+    width: 100%;
+    height: 150px;
+    border-radius: 10px;
+    margin: 1rem 0;
+    padding: 0;
+
+    img {
+      width: 40%;
+      height: 150px;
+      border-right: 1px solid $primary;
+      border-radius: 10px 0px 0px 10px;
+      // object-fit: contain;
+    }
+
+    ._details {
+      padding: 0.5rem;
+
+      a {
+        font-size: 18px;
+        font-weight: 600;
+      }
+
+      span {
+        font-size: 16px;
+      }
     }
   }
 }
