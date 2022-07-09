37c0d5a2c3e5070737f70fecca1107fbcf282c91
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-09 10:23:50 +0100

    added mock products

--- components/App/Cards/Product.vue
+++ components/App/Cards/Product.vue
@@ -1,5 +1,13 @@
 <template>
-  
+  <div class="product">
+    <div class="s">
+      <img loading="lazy" src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1650226544/tec/products/edited_image_with_label_vrbcdx.png" alt="">
+      <div class="_details">
+        <NuxtLink to="/shop/10-inch-Double-Layer-Whipped-Cream-Cake-GWER435T">10 inch Double Layer Whipped Cream Cake</NuxtLink>
+        <span class="prices">₦20,500 - ₦27,500</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +16,6 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
