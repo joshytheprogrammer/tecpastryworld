d4ecf120ada7f9fb8de83105e615f3c7883c07a3
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-11 23:10:26 +0100

    made notifications component

--- components/App/Helpers/Global/Notification.vue
+++ components/App/Helpers/Global/Notification.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="notification">
-    
+    <p>
+      <!-- The following error has occured : {{message}} -->
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-
+  
 }
 </script>
 
 <style lang="scss" scoped>
+.notification {
 
+  &.success {
+    
+  }
+
+  &.warning {
+
+  }
+
+  &.error {
+
+  }
+}
 </style>
