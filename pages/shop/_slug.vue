7e241645514369ec3accf3060ddfafa31b0a9d71
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-11 18:24:10 +0100

    shop/slug

--- pages/shop/_slug.vue
+++ pages/shop/_slug.vue
@@ -1,13 +1,25 @@
 <template>
-  
+  <div>
+    Hello World
+    {{this.$route.params.slug}}
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      product: null,
+    }
+  },
+  // async fetch() {
+  //   await axios.get('http://127.0.0.1:8000/api/shop/'+this.$route.params.slug).then((response) => {
+  //     this.product = response.data
+  //   })
+  // }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
