d5ee168abe8ab671f2afdbf82d3826cef9e106d8
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-11 19:23:52 +0100

    got shop to work

--- pages/shop/_slug.vue
+++ pages/shop/_slug.vue
@@ -1,22 +1,12 @@
 <template>
-  <div class="">
-    Hello World
-    {{this.$route.params.slug}}
+  <div class="product">
+    
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      product: null,
-    }
-  },
-  // async fetch() {
-  //   await axios.get('http://127.0.0.1:8000/api/shop/'+this.$route.params.slug).then((response) => {
-  //     this.product = response.data
-  //   })
-  // }
+  
 }
 </script>
 
