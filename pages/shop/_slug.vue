02746d875de094af76fe69da067a51c455aa0661
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-11 22:17:30 +0100

    created notification component

--- pages/shop/_slug.vue
+++ pages/shop/_slug.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="product">
-    
+    <div class="item" v-if="product">
+      {{this.product}}
+    </div>
+    <div v-else>
+      Loading ...
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios"
 export default {
-  
+  data() {
+    return {
+      product: null
+    }
+  },
+  mounted(){
+    this.$fetch()
+  },
+  async fetch() {
+    await axios.get('http://127.0.0.1:8000/api/shop/'+this.$route.params.slug).then((response) => {
+      this.product = response.data.data[0]
+    })
+  }
 }
 </script>
 
