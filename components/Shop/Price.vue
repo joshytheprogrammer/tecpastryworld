d9a187641bc583c36f1b8e7b7077c86df21e3bf2
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-14 21:02:40 +0100

    replaced price

---
+++ components/Shop/Price.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="price">
+    <p>₦14,000</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.price {
+  background: $background;
+  border-radius: 10px;
+  border: 1px solid $dark;
+  width: fit-content;
+  margin: 1rem 0;
+  font-size: 28px;
+  font-weight: 400;
+
+  p {
+    padding: 0.6rem 1rem;
+  }
+}
+</style>
