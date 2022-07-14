facd9561050e4c24e0f741ce43eae1d90568f0f5
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-14 11:09:46 +0100

    created network error component

---
+++ components/App/Helpers/Global/Error.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="error" v-if="message">
+    <p class="message">{{message}}.. Try <span @click="() => {$nuxt.refresh()}">Refreshing</span></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message']
+}
+</script>
+
+<style lang="scss" scoped>
+.error {
+  padding: 1rem;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+
+  .message {
+    padding: 0.5rem;
+    background: $danger;
+    border-radius: 4px;
+    color: $light;
+    font-weight: 700;
+    font-size: 12px;
+
+    span {
+      text-decoration: underline;
+      cursor: pointer;
+    }
+  }
+}
+</style>
