63cb9823d529622831f62f083e4e19586d0a7da3
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-16 17:28:00 +0100

    created noForm component

--- components/Shop/NoForm.vue
+++ components/Shop/NoForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m">
-    <p>Product not available online</p>
+    <p>Product not eligible for online purchase.</p>
     <button>Whatsapp to Order</button>
   </div>
 </template>
@@ -12,5 +12,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.m {
 
+  p {
+    background: $warning;
+    color: $primary;
+    padding: 0.5rem 1rem;
+    margin: 1rem 0;
+    font-size: 16px;
+    font-weight: 400;
+    border-radius: 4px;
+    width: fit-content;
+  }
+
+  button {
+    background: $success;
+    color: $primary;
+    border: none;
+    font-size: 16px;
+    font-weight: 400;
+    border-radius: 8px;
+    height: 45px;
+    padding: 4px 24px;
+    margin: 1rem 0;
+    cursor: pointer;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    transition: box-shadow 0.4s ease-in-out;
+
+    &:hover {
+      box-shadow: 0px 4px 4px $dark;
+    }
+  }
+}
 </style>
