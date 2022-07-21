f010df0ed1e9ec9470757ca49335814702c040d9
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-21 16:06:33 +0100

    made changes: 'message bar'

--- pages/cart.vue
+++ pages/cart.vue
@@ -2,15 +2,19 @@
   <div class="cart">
     <div class="items">
       <h1>Shopping Cart</h1>
-      <div v-for="item in cartItems" :key="item.id">
-        <p> {{item.id}} </p>
-        <p> {{item.img}} </p>
-        <p> {{item.name}} </p>
-        <p> {{item.data.size}} </p>
-        <p> {{item.data.type}} </p>
-        <p> {{item.data.price}} </p>
-        <p> {{item.data.message}} </p>
-        <hr><br>
+      <div class="item" v-for="item in cartItems" :key="item.id">
+        <div class="_thumbnail">
+          <img width="120px" height="120px" :src="item.img" :alt="'Cart Item - '+item.name">
+        </div>
+        <div class="_details">
+          <p> {{item.name}} </p>
+          <p> {{item.data.size}} </p>
+          <p> {{item.data.type}} </p>
+          <p> {{item.data.message}} </p>
+        </div>
+        <div class="_price">
+          <p> {{item.data.price}} </p>
+        </div>
       </div>
     </div>
     <div class="checkout">
@@ -40,15 +44,30 @@ export default {
   display: grid;
   grid-template-columns: 2fr 1fr;
 
-  h1 {
-    width: 100%;
-    padding: 40px 1rem;
+  .items {
     background: $light;
-    border-bottom: 1px solid $primary;
-    text-align: left;
+    border-right: 1px solid $primary;
+    width: 100%;
+    
+    h1 {
+      border-bottom: 1px solid $primary;
+      padding: 40px 1rem;
+      
+      text-align: left;
 
-    font-size: 36px;
-    font-weight: 400;
+      font-size: 36px;
+      font-weight: 400;
+    }
+
+    .item {
+      display: grid;
+      grid-template-columns: 1fr 2fr 1fr;
+    }
+  }
+
+  .checkout {
+    margin-left: 0.5rem;
+    background: $light;
   }
 }
 </style>
