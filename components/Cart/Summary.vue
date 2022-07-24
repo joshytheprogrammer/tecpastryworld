c5c872e796644baf5060bfc971402b10a76b8ea0
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-24 19:32:09 +0100

    made changes: ''

--- components/Cart/Summary.vue
+++ components/Cart/Summary.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="labels">
     <div class="label">
-      <span class="name"></span>
-      <h6 class="value"></h6>
+      <span class="name">Subtotal (3 items)</span>
+      <h6 class="value">₦20,500</h6>
     </div>
+    <button>Proceed to checkout</button>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
 
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.labels {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
+  .label {
+
+  }
+}
 </style>
