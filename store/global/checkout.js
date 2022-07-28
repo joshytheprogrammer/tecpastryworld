1bdac72eac61908f571f86951077e3ec3f4a8199
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-28 10:02:28 +0100

    made changes: 'attempting to integrate checkout with backend'

---
+++ store/global/checkout.js
@@ -0,0 +1,17 @@
+export default {
+  namespaced: true,
+  state : () => ({
+    checkout: {
+      
+    }
+  }),
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  getters: {
+
+  }
+}
---
+++ store/global/user.js
@@ -0,0 +1,18 @@
+export default {
+  namespaced: true,
+  state : () => ({
+    user: {
+      id: null,
+      phone: null,
+    }
+  }),
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  getters: {
+
+  }
+}
