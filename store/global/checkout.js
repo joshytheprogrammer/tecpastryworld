96312104fae4da84b0e7e65e8c45c88ea24b5373
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-28 10:15:10 +0100

    made changes: 'attempting to integrate checkout with backend'

--- store/global/checkout.js
+++ store/global/checkout.js
@@ -1,17 +1,40 @@
 export default {
   namespaced: true,
   state : () => ({
-    checkout: {
-      
-    }
+    order: {
+      phone: null,
+      amount: null,
+      payment_mode: null,
+      fulfillment_method: null,
+      products: []
+    },
   }),
   mutations: {
 
   },
   actions: {
+    initiateCheckout() {
 
+    },
+    addPhone({ commit, dispatch }, phone) {
+
+    },
+    addFulfillment({commit, dispatch}, method) {
+
+    },
+    addPayment({commit, dispatch}, method) {
+      
+    }
   },
   getters: {
+    returnPhone(state) {
 
+    },
+    returnFulfillment(state) {
+
+    },
+    returnPayment(state) {
+      
+    }
   }
 }
