64558888f559c779784817cc00e1408acff19c3c
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-08-01 10:51:29 +0100

    made changes: ''

--- pages/order.vue
+++ pages/order.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="order">
-    
+    <Search />
     Your order {{$route.query.order_no}} was successfully placed.
   </div>
 </template>
 
 <script>
+import Search from "../components/Order/Search.vue"
 export default {
-
+  components: {
+    Search
+  }
 }
 </script>
 
