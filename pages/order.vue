8d9e9a531450ae0025d5c586cd42b5410681d9ca
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-08-02 00:06:01 +0100

    made changes: ''

--- pages/order.vue
+++ pages/order.vue
@@ -8,10 +8,7 @@
         <h2>Order Details</h2>
         <div class="data">
           <Details :order="order" />
-          <div class="res">
-            <p>If you have made payment, we will reach out to you soon. If you want to talk to us, you can reach out to us <a :href="'https://api.whatsapp.com/send?phone=2347010718819&text=Hi, I placed the order ['+order_no+']. How far?'">here</a>. </p>
-            <p>Be sure to reach out to us with the <b :title="order.customer_phone">PHONE NUMBER</b> you used to order.</p>
-          </div>
+          
         </div>
       </div>
       <div class="container products">
