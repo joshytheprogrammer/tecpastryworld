ab4539e7d47c2a01a92c1cdc1deae877910db98c
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-08-01 10:55:40 +0100

    made changes: ''

--- pages/order.vue
+++ pages/order.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="order">
     <Search />
-    Your order {{$route.query.order_no}} was successfully placed.
+    <div class="content" v-show="$route.query.order_no">
+
+    </div>
+    Your order {{order_no}} was successfully placed.
   </div>
 </template>
 
@@ -10,6 +13,16 @@ import Search from "../components/Order/Search.vue"
 export default {
   components: {
     Search
+  },
+  watch: {
+    '$route.query.order_no': function (no){
+      this.order_no = no
+    }
+  },
+  data() {
+    return {
+      order_no: this.$route.query.order_no
+    }
   }
 }
 </script>
