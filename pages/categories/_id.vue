7e5f8618943a909f5e1796f11727cd09dfc97b15
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-09 11:41:52 +0100

    made changes

--- pages/categories/_id.vue
+++ pages/categories/_id.vue
@@ -33,7 +33,6 @@ export default {
   async fetch() {
     await axios.get('http://127.0.0.1:8000/api/categories/'+this.$route.params.id+'/products').then((response) => {
       this.products.push(...response.data.data)
-      console.log(response)
     }).catch((error) => {
       this.error = error.message
     })
