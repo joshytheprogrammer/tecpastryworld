0e32f6c32e266fce4d5c6c077e5147403bb2c14e
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-09-09 10:59:39 +0100

    made changes

--- components/Search/Results.vue
+++ components/Search/Results.vue
@@ -1,15 +1,7 @@
 <template>
   <div class="results">
     <div class="products">
-      <div class="product" v-for="item in results.products" :key="item._id">
-        <div class="s">
-          <img loading="lazy" :src="item.thumbnail" :alt="item.name">
-          <div class="_details">
-            <NuxtLink :to="'/shop/'+item._slug" :aria-label="item.name" :title="item.name">{{item.name}}</NuxtLink>
-            <span class="prices"> [ {{formatter(item.price)}} ] </span>
-          </div>
-        </div>
-      </div>
+      <Product v-for="item in results.products" :key="item._id" :item="item" />
     </div>
     <div class="categories">
       <div class="category" v-for="item in results.categories" :key="item.id">
@@ -25,8 +17,12 @@
 </template>
 
 <script>
+import Product from "../App/Cards/Product.vue"
 export default {
   props: ["results"],
+  components: {
+    Product
+  },
   methods: {
     formatter(price) {
       price = JSON.parse(price)
@@ -56,86 +52,6 @@ export default {
     display: grid;
     justify-items: center;
     grid-template-columns: repeat(4, 1fr);
-
-
-    .product {
-      padding: 1rem 0;
-
-      .s {
-        img {
-          width: 100%;
-          max-height: 100%;
-
-          height: 300px;
-          object-fit: contain;
-          border-radius: 10px;
-          transition: 0.5s all ease-in-out;
-        }
-
-        ._details {
-          display: block;
-          padding: 1rem 0.5rem;
-          color: $primary;
-
-          a {
-            padding: 0.2rem 0;
-            text-transform: capitalize;
-            display: inherit;
-            color: inherit;
-            text-decoration: none;
-            font-size: 21px;
-            font-weight: 600;
-
-            &:hover {
-              text-decoration: underline;
-              text-decoration-color: $primary;
-            }
-          }
-
-          span {
-            font-size: 18px;
-            font-weight: 400;
-          }
-        }
-      }
-
-      @media screen and (max-width: $s-medium) {
-        padding: 0;
-        margin: 1rem 0;
-        width: 100%;
-
-        .s {
-          display: grid;
-          grid-template-columns: 1fr 2fr;
-          border: 1px solid $primary;
-
-          height: 150px;
-          border-radius: 10px;
-
-          img {
-            width: 100%;
-            height: 150px;
-            object-fit: contain;
-            border-right: 1px solid $primary;
-            border-radius: 10px 0px 0px 10px;
-          }
-
-          ._details {
-            padding: 0.5rem;
-
-            a {
-              height: auto;
-              font-size: 18px;
-              font-weight: 600;
-            }
-
-            span {
-              font-size: 16px;
-            }
-          }
-        }
-      }
-    }
 
     @media screen and (max-width: $large) {
       width: 100%;
