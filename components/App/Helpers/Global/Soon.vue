0613782ed8ef3ddbd951cc18cb00fb91bccb3257
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-10-27 14:00:09 +0100

    made changes

--- components/App/Helpers/Global/Soon.vue
+++ components/App/Helpers/Global/Soon.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="shop">
-    Coming Soon
-  </div>
+  
 </template>
 
 <script>
@@ -10,6 +8,6 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style>
 
 </style>
