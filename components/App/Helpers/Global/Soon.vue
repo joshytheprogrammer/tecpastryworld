c9008461ae21653dd4dce2c6dd77a8b0d26431d4
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-10-27 14:18:06 +0100

    made changes

--- components/App/Helpers/Global/Soon.vue
+++ components/App/Helpers/Global/Soon.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="coming">
     <h1>Coming Soon</h1>
-    <p>We are constantly improving our user experience</p>
+    <p>Hi there 👋. The page you are looking for is currently undergoing development. We'll have it ready as soon as possible.</p>
   </div>
 </template>
 
@@ -12,5 +12,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.coming {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  align-content: center;
+  align-items: center;
 
+  padding: 2rem 0;
+
+  h1 {
+    font-size: 10vw;
+    color: $primary;
+  }
+
+  p {
+    font-size: 24px;
+  }
+}
 </style>
